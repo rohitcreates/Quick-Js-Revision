536343cfdca5dda46d5b8c87ffeb01f03474441d
Rohit <167241336+rohitcreates@users.noreply.github.com>
2024-11-22 18:31:49 +0000

    datatypes by chai or code

--- 01_basics/01_dataType.js
+++ 01_basics/01_dataType.js
@@ -1,5 +1,3 @@
-"use strict"; // treat all JS code as newer version
-
 let js = 23; // js is now number
 console.log(typeof js);
 
@@ -26,4 +24,34 @@ console.log(typeof js);
 
 js = Symbol('Id'); // js is now Symbol
 console.log(typeof js);
+
+// Chai or Code 
+
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello"); // we are using node js, not browser
+
+console.log(3 
+    + 3); // code readability should be high
+console.log("Rohit");
+
+let name = "Rohit";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+
+// number => 2 to power 53
+// bigInt
+// String => ""
+// boolean => true/false
+// null => Standalone value
+// undefined =>
+// symbol => unique 
+
+
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // null
 
