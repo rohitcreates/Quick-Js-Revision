d35cf04ad7f7ed0e860b98dccf436721442b86bb
Rohit <167241336+rohitcreates@users.noreply.github.com>
2024-11-22 18:08:44 +0000

    All DataTypes

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all JS code as newer version
+
+let js = 23; // js is now number
+js = "JavaScript"; // js is now String
+js = true; // js is now boolean
+js = {}; // js is now object
+js = []; // js is now array
+js = ""; // js is now undifined
+js = null; // js is now null
+js = 1234567890123456789012345678901234567890n; // BigInt
+js = Symbol('Id'); // js is now Symbol
